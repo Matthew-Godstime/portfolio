abbc0d84d8e595f852dbcb910391d4466ba8e78e
Matthew God'stime <matthewgodstime8585@gmail.com>
2022-06-28 21:47:04 +0100

    fully finished portfolie dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
